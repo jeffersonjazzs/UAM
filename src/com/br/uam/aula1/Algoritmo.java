d33f979c74f8903dd50b6220e54ef4ae58d3bcde
Jeffersonjazzs <jefferson_jazzs@hotmail.com>
2019-07-30 21:27:19 -0300

    Aula 1

---
+++ src/com/br/uam/aula1/Algoritmo.java
@@ -0,0 +1,20 @@
+package com.br.uam.aula1;
+
+import java.util.Scanner;
+
+public class Algoritmo {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int primeiro, segundo;
+        char a, e, i, o, u;
+        System.out.println("digite o primeiro numero");
+        primeiro = entrada.nextInt();
+        System.out.println("digite o segundo numero");
+        segundo = entrada.nextInt();
+
+        System.out.println("escolha sua operação + - * / ");
+        a = entrada.next().charAt(0);
+
+    }
+}
